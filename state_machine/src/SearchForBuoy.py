fb649133a0fd96889547b3160bb1c209de6e1ffc
ArenPetrossian <apetros4@calstatela.edu>
2022-08-14 14:54:27 -0700

    SearchForBuoy Comp. Updated

--- state_machine/src/SearchForBuoy.py
+++ state_machine/src/SearchForBuoy.py
@@ -17,58 +17,55 @@ class SearchForBuoy(smach.State):
 		self.cv_sub = Subscribe_to('target')
 		self.sensors_sub = Subscribe_to('sensorInfo_actuatorStatus')
 		self.counter = 0
+		self.gate_detected = False
 		self.task = task_desiredAction()
 		time.sleep(2)
 
+
 	def execute(self, userdata):
 		self.task.currentState = "Search: Buoy"
-		self.smach_pub.publish(self.task)
-		#Assume Robot starts with all setpoints at 0
 		print("Dive")
 		self.task.depth_set = 1
 		self.smach_pub.publish(self.task)
-		time.sleep(0.5)
-		self.sensors_data = self.sensors_sub.get_data()
-		while not self.sensors_data.stabilized:
-			time.sleep(0.01)
-			self.counter = self.counter + 1
-			if (self.counter > 3000): #was originally 15
-				return 'Failed'
-			self.sensors_data = self.sensors_sub.get_data()
-
-		#Turn 45
-		print("Turn 45")
-		self.counter = 0
 		self.task.depth_set = 0
-		self.task.yaw_set = 45
-		self.smach_pub.publish(self.task)
-		time.sleep(0.5)
+		time.sleep(3)
+
 		self.sensors_data = self.sensors_sub.get_data()
-		while not self.sensors_data.stabilized:
+		while self.sensors_data.stabilized_time == 0:
 			time.sleep(0.01)
 			self.counter = self.counter + 1
 			if (self.counter > 3000): #was originally 15
 				return 'Failed'
 			self.sensors_data = self.sensors_sub.get_data()
 
-		#Forward until see Buoy
-		print("Forwards")
-		self.counter = 0
-		self.task.yaw_set = 0
-		self.task.distance_set = 5
-		self.smach_pub.publish(self.task)
 		self.cv_data = self.cv_sub.get_data()
-		while not self.cv_data.buoy1:
-			time.sleep(0.01)
-			self.counter = self.counter + 1
-			if (self.counter > 3000): #was originally 20
-				return 'Failed'
-			self.cv_data = self.cv_sub.get_data()
+		for target in self.cv_data.targets:
+			if target.name == 'Gate_GMan':
+				self.gate_detected = True
+		if not self.gate_detected:
+			rotation = 720
+			angle_increment = 30
+			counter = 0
+			rate = rospy.Rate(20)
+			while counter <= (rotation / angle_increment) and not self.gate_detected:
+				self.sensors_data = self.sensors_sub.get_data()
+				if self.sensors_data.stabilized_time > 0:
+					self.task.yaw_set = angle_increment
+					print (self.task.yaw_set + self.sensors_data.yaw_current)
+					self.smach_pub.publish(self.task)
+					#time.sleep(0.2) might be needed
+					counter += 1
 
-		print("Buoy Found")
-		self.task.distance_set = 0
-		self.smach_pub.publish(self.task)
-		return 'Success'
+				self.cv_data = self.cv_sub.get_data()
+				for target in self.cv_data.targets:
+					if target.name == 'Gate_GMan':
+						self.gate_detected = True
+				rate.sleep()
+
+		if self.gate_detected:
+			return 'Success'
+
+		return 'Failed'
 
 
 def code():
